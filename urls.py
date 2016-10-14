b0755d692201bb0ad7e89989ea290a898cb3497f
Liz Walsh <liz@litazia.com>
2016-10-13 21:17:09 -0400

    fix

--- urls.py
+++ urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^admin/' admin.site.urls),
     url(r'^news/', include('news.urls', namespace="news")),
     url(r'^archive/', include('archive.urls', namespace="archive")),
+     url(r'^extras/', include('extras.urls', namespace="photologue")),
 ]
