0c2ccf7291cdd14ea9db474a95e8edf4ec5e3279
Liz Walsh <liz@litazia.com>
2017-04-13 20:47:59 -0400

    Custom management command

--- comics/management/commands/reminder.py
+++ comics/management/commands/reminder.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from comics.models import Comic, ComicFile, UploadedComicFile
+from comics.models import Comic
 from django.utils.dateparse import parse_date
 from datetime import datetime
 from django.conf import settings
@@ -9,16 +9,19 @@ class Command(BaseCommand):
     help = "Publishes the next available comic (on today's date)"
     
     def add_arguments(self, parser):
-        parser.add_argument('date', nargs='?', default=datetime.today())
+        #parser.add_argument('date', nargs='?', default=datetime.today())
+        parser.add_argument('tod')
         
     def handle(self, *args, **options):
-        thedate = options['date']
+        thedate = datetime.today()
+        tod = options['tod']
         try:
             comic = Comic.objects.get(date=thedate)
+            comic.tweet_comic_reminder(tod)
         except Comic.DoesNotExist:
             raise CommandError('Comic for %s does not exist' % str(day))
             
         # commented out all the old stuff
-        result = comic.publish()
-        if result == True:
-            comic.tweet_comic()
+        #result = comic.publish()
+        #if result == True:
+            #comic.tweet_comic_reminder()
